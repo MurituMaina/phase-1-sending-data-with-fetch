425acef1bd873eab8764286088772be4a69ad3c5
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-27 15:28:13 +0300

    completed assginment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+let name = "Steve"
+    let email = "steve@steve.com"
+ function submitData(name, email){
+    
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'Accept': 'application/json'
+        },
+        body:JSON.stringify({name: `${name}`, email: `${email}`})
+    })
+    .then(resp => resp.json())
+    .then(user => {
+        // console.log(user.id)
+        let idValue = document.createElement('p');
+       idValue.innerHTML =`${user.id}`;
+        let ouputValue = document.querySelector('body');
+        ouputValue.append(idValue.innerHTML);
+    })
+    .catch((error) => {
+        let errorMessage =document.createElement("p");
+        errorMessage.textContent = error.message
+        let errorOuput = document.querySelector("body")
+        errorOuput.append(errorMessage)
+    
+    })
+    }
